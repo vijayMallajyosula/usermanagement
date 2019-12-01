dbe8a34f5b6d8965b40c37004dc7dd4136360a9a
unknown <m.vijayshyamc@gmail.com>
2019-12-01 09:38:42 +0530

    Commiting first code

---
+++ bookreadingtracker/src/main/java/com/trackingapp/platform/BookreadingtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.trackingapp.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookreadingtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookreadingtrackerApplication.class, args);
+	}
+
+}
